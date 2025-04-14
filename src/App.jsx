606bad74f9168f6054149940adea61b97ca7a735
Vipul <vipulkhanvilkar@gmail.com>
2025-04-14 19:19:03 +0530

    added only image

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ const App = () => {
   return (
     <main className="overflow-x-hidden bg-white dark:bg-black text-black dark:text-white duration-300">
       <Navbar />
+      
       <Hero togglePlay={togglePlay} />
       <Quotes />
       <Banner togglePlay={togglePlay} />
